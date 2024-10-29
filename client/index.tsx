cf275d1bbbf8098bc7cc514e3e7a7ddeaa76c2da
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-10-28 20:17:36 -0400

    added react router and refactored index into dashboard and login

--- client/index.tsx
+++ client/index.tsx
@@ -1,64 +1,31 @@
 import * as ReactDOM from 'react-dom/client';
-import { useState } from 'react';
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import LoginForm from './login';
+import Dashboard from './dashboard';
 import './styles.css';
 
 const root = document.getElementById('root')!;
-
 const reactRoot = ReactDOM.createRoot(root);
 
-reactRoot.render(<App />);
-
-function App() {
-  const [Username, setUsername] = useState("username");
-  const [Password, setPassword] = useState("password");
-
-  return (
-    <>   
-      <LoginForm setUsername={setUsername} setPassword={setPassword} />
-      <h1>{Username}</h1>
-      <h1>{Password}</h1>
-    </>
-  );
-}
-
-interface LoginFormProps {
-  setUsername: React.Dispatch<React.SetStateAction<string>>;
-  setPassword: React.Dispatch<React.SetStateAction<string>>;
-}
-
-function LoginForm({ setUsername, setPassword }: LoginFormProps) {
-    function doLogin(event: React.FormEvent<HTMLFormElement>) {
-      event.preventDefault(); 
-      console.log("Form submitted");
-    }
-  
-    function updateUsername(event: React.ChangeEvent<HTMLInputElement>) {
-      setUsername(event.target.value); 
-    }
-  
-    function updatePassword(event: React.ChangeEvent<HTMLInputElement>) {
-      setPassword(event.target.value); 
-    }
-  
-    return (
-      <form onSubmit={doLogin}>
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            onChange={updateUsername}
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={updatePassword}
-          />
-        </div>
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    );
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:
+      <>
+        <LoginForm />
+        <Link to={'/dashboard'}>go to dashboard</Link>
+      </>
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
   }
+]);
+
+reactRoot.render(<>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+</>);
+
---
+++ client/login.tsx
@@ -0,0 +1,36 @@
+export default function LoginForm() {
+  function doLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("Form submitted");
+  }
+
+  function updateUsername(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value);
+  }
+
+  function updatePassword(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value);
+  }
+
+  return (
+    <form onSubmit={doLogin}>
+      <div>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={updateUsername}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={updatePassword}
+        />
+      </div>
+      <div>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  );
+}
