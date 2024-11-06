38acac2a4db84204b147bc95841fd78d7dca6eec
Bea <beatriz.altimeyer@gmail.com>
2024-11-06 15:39:31 -0500

    possible logo added and background back changed

--- client/login.tsx
+++ client/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Make sure to import Link
+import { Link } from 'react-router-dom'; 
 
 export default function LoginForm() {
   function doLogin(event: React.FormEvent<HTMLFormElement>) {
