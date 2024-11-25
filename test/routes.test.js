75063385bfd47a836ed218359df186b37c05403d
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-11-25 17:34:48 -0500

    finished unit test

--- test/routes.test.js
+++ test/routes.test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const request = require('supertest');
 const express = require('express');
 const registerHandler = require('../api/register');
+const loginHandler = require('../api/login');
 const mocha = require('mocha');
 const crypt = require('node:crypto');
 const dotenv = require("dotenv");
@@ -20,7 +21,7 @@ mocha.describe('FocusFins App Registration', (m) => {
 
 	mocha.it('should respond with a 201 status code with a message when registering', (done) => {
 		const payload = {
-			username: 'user',
+			username: 'example',
 			password: 'password',
 			firstName: 'John',
 			lastName: 'Doe',
@@ -42,17 +43,20 @@ mocha.describe('FocusFins App Login', (m) => {
 		req.secretToken = SECRET_TOKEN;
 		return next();
 	});
-	app.use('/api/register', registerHandler);
+	app.use('/api/login', loginHandler);
 
-	mocha.it('should respond with a 201 status code with a message when registering', (done) => {
+	mocha.it('should respond with a 200 status code with a token when logging in', (done) => {
 		const payload = {
 			username: 'test',
 			password: 'user',
 		};
-		request(app).post('/api/register').send(payload).set('Content-Type', 'application/json').accept('Accept', 'application/json').end((err, res) => {
+		request(app).post('/api/login').send(payload).set('Content-Type', 'application/json').accept('Accept', 'application/json').end((err, res) => {
 			if (err) return done(err);
-			expect(res.status).to.equal(201);
-			expect(res.body.message).to.equal("User registered successfully");
+			expect(res.status).to.equal(200);
+			expect(res.body.token).to.not.equal(undefined);
+			expect(res.body.id).to.not.equal(undefined);
+			expect(res.body.firstName).to.not.equal(undefined);
+			expect(res.body.lastName).to.not.equal(undefined);
 			done();
 		});
 	});
