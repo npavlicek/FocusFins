6e652a771993b06e93268c2d0f01cd6ea1939aea
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-11-24 23:52:13 -0500

    updates

--- client/dashboard.tsx
+++ client/dashboard.tsx
@@ -4,183 +4,183 @@ import { StrictMode, useState, useCallback, useRef, useEffect } from 'react';
 import { CoralCallbacks, CoralData } from './coral';
 
 interface DashboardParams {
-  id: string;
-  token: string;
+	id: string;
+	token: string;
 };
 
 export default function Dashboard(props: DashboardParams) {
-  const [cursorAvail, setCursorAvail] = useState(true);
-  const [corals, setCoralsData] = useState<CoralData[]>([]);
-  const [currentCoralId, setCurrentCoralId] = useState<number>(0);
-  const popupRef = useRef<HTMLDivElement>(null);
-  const moveButtonRef = useRef<HTMLButtonElement>(null);
-  const rotateButtonRef = useRef<HTMLButtonElement>(null);
-  const deleteButtonRef = useRef<HTMLButtonElement>(null);
-  const [error, setError] = useState<boolean>();
+	const [cursorAvail, setCursorAvail] = useState(true);
+	const [corals, setCoralsData] = useState<CoralData[]>([]);
+	const [currentCoralId, setCurrentCoralId] = useState<number>(0);
+	const popupRef = useRef<HTMLDivElement>(null);
+	const moveButtonRef = useRef<HTMLButtonElement>(null);
+	const rotateButtonRef = useRef<HTMLButtonElement>(null);
+	const deleteButtonRef = useRef<HTMLButtonElement>(null);
+	const [error, setError] = useState<boolean>();
 
-  useEffect(() => {
-    if (popupRef.current) {
-      popupRef.current.style.display = "none";
-    }
+	useEffect(() => {
+		if (popupRef.current) {
+			popupRef.current.style.display = "none";
+		}
 
-    const getCoralsReqBody = JSON.stringify({
-      id: props.id
-    });
+		const getCoralsReqBody = JSON.stringify({
+			id: props.id
+		});
 
-    fetch('/api/getCorals', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: getCoralsReqBody
-    }).then(res => {
-      if (res.status === 200) {
-        res.json().then(data => {
-          setCoralsData(data.corals);
-          setCurrentCoralId(data.currentCoralIdx);
-        });
-      } else {
-        setError(true);
-      }
-    });
-  }, []);
+		fetch('http://focusfins.org/api/getCorals', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: getCoralsReqBody
+		}).then(res => {
+			if (res.status === 200) {
+				res.json().then(data => {
+					setCoralsData(data.corals);
+					setCurrentCoralId(data.currentCoralIdx);
+				});
+			} else {
+				setError(true);
+			}
+		});
+	}, []);
 
-  /**
-   * push entire coral state and override on database
-   */
-  const pushCorals = (newCorals: CoralData[]) => {
-    const reqBody = JSON.stringify({
-      token: props.token,
-      corals: newCorals
-    });
+	/**
+	 * push entire coral state and override on database
+	 */
+	const pushCorals = (newCorals: CoralData[]) => {
+		const reqBody = JSON.stringify({
+			token: props.token,
+			corals: newCorals
+		});
 
-    fetch('./api/updateCorals', {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: reqBody
-    }).then(res => {
-      if (res.status !== 200) {
-        setError(true);
-      }
-    });
-  };
+		fetch('http://focusfins.org/api/updateCorals', {
+			method: 'post',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: reqBody
+		}).then(res => {
+			if (res.status !== 200) {
+				setError(true);
+			}
+		});
+	};
 
-  /**
-   * remove a coral by id
-   */
-  const pushRemoveCoral = (coralId: number) => {
-    const reqBody = JSON.stringify({
-      token: props.token,
-      coralId: coralId
-    });
+	/**
+	 * remove a coral by id
+	 */
+	const pushRemoveCoral = (coralId: number) => {
+		const reqBody = JSON.stringify({
+			token: props.token,
+			coralId: coralId
+		});
 
-    fetch('./api/removeCoral', {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: reqBody
-    }).then(res => {
-      if (res.status !== 200) {
-        setError(true);
-      }
-    });
-  };
+		fetch('http://focusfins.org/api/removeCoral', {
+			method: 'post',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: reqBody
+		}).then(res => {
+			if (res.status !== 200) {
+				setError(true);
+			}
+		});
+	};
 
-  /**
-   * update coral by id 
-   */
-  const pushUpdateCoral = (coralData: CoralData) => {
-    const reqBody = JSON.stringify({
-      token: props.token,
-      coralId: coralData.coralId,
-      coral: coralData
-    });
+	/**
+	 * update coral by id
+	 */
+	const pushUpdateCoral = (coralData: CoralData) => {
+		const reqBody = JSON.stringify({
+			token: props.token,
+			coralId: coralData.coralId,
+			coral: coralData
+		});
 
-    fetch('./api/updateCoral', {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: reqBody
-    }).then(res => {
-      if (res.status !== 200) {
-        setError(true);
-      }
-    });
-  };
+		fetch('http://focusfins.org/api/updateCoral', {
+			method: 'post',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: reqBody
+		}).then(res => {
+			if (res.status !== 200) {
+				setError(true);
+			}
+		});
+	};
 
-  const updateCoral = useCallback((newCoralData: CoralData) => {
-    pushUpdateCoral(newCoralData);
-    setCoralsData(oldCoralData => {
-      return oldCoralData.map(val => {
-        if (val.coralId === newCoralData.coralId) {
-          return newCoralData;
-        } else {
-          return val;
-        }
-      });
-    });
-  }, [corals, pushCorals]);
+	const updateCoral = useCallback((newCoralData: CoralData) => {
+		pushUpdateCoral(newCoralData);
+		setCoralsData(oldCoralData => {
+			return oldCoralData.map(val => {
+				if (val.coralId === newCoralData.coralId) {
+					return newCoralData;
+				} else {
+					return val;
+				}
+			});
+		});
+	}, [corals, pushCorals]);
 
-  const deleteCoral = useCallback((coralId: number) => {
-    pushRemoveCoral(coralId);
-    setCoralsData(oldCorals => {
-      return oldCorals.filter((val) => {
-        if (val.coralId != coralId)
-          return true;
-        else
-          return false;
-      });
-    });
-  }, [corals, pushCorals]);
+	const deleteCoral = useCallback((coralId: number) => {
+		pushRemoveCoral(coralId);
+		setCoralsData(oldCorals => {
+			return oldCorals.filter((val) => {
+				if (val.coralId != coralId)
+					return true;
+				else
+					return false;
+			});
+		});
+	}, [corals, pushCorals]);
 
-  const createPopup = useCallback((x: number, y: number, coralCallbacks: CoralCallbacks) => {
-    if (popupRef.current) {
-      popupRef.current.style.position = 'absolute';
-      popupRef.current.style.display = 'block';
-      popupRef.current.style.top = (y - 100).toString() + 'px';
-      popupRef.current.style.left = x.toString() + 'px';
-      popupRef.current.style.zIndex = '100';
-      if (moveButtonRef.current)
-        moveButtonRef.current.onclick = coralCallbacks.moveButtonHandler;
-      if (rotateButtonRef.current)
-        rotateButtonRef.current.onclick = coralCallbacks.rotateButtonHandler;
-      if (deleteButtonRef.current)
-        deleteButtonRef.current.onclick = coralCallbacks.deleteButtonHandler;
-    }
-  }, []);
+	const createPopup = useCallback((x: number, y: number, coralCallbacks: CoralCallbacks) => {
+		if (popupRef.current) {
+			popupRef.current.style.position = 'absolute';
+			popupRef.current.style.display = 'block';
+			popupRef.current.style.top = (y - 100).toString() + 'px';
+			popupRef.current.style.left = x.toString() + 'px';
+			popupRef.current.style.zIndex = '100';
+			if (moveButtonRef.current)
+				moveButtonRef.current.onclick = coralCallbacks.moveButtonHandler;
+			if (rotateButtonRef.current)
+				rotateButtonRef.current.onclick = coralCallbacks.rotateButtonHandler;
+			if (deleteButtonRef.current)
+				deleteButtonRef.current.onclick = coralCallbacks.deleteButtonHandler;
+		}
+	}, []);
 
-  const closePopup = useCallback(() => {
-    if (popupRef.current) {
-      popupRef.current.style.display = 'none';
-    }
-  }, []);
+	const closePopup = useCallback(() => {
+		if (popupRef.current) {
+			popupRef.current.style.display = 'none';
+		}
+	}, []);
 
-  return (
-    <StrictMode>
-      <div id="dashboard">
+	return (
+		<StrictMode>
+			<div id="dashboard">
 
-        <div ref={popupRef} className="popup-container">
-          <button ref={moveButtonRef} className="popup-button">move</button>
-          <button ref={rotateButtonRef} className="popup-button">rotate</button>
-          <button ref={deleteButtonRef} className="popup-button-delete">delete</button>
-        </div>
+				<div ref={popupRef} className="popup-container">
+					<button ref={moveButtonRef} className="popup-button">move</button>
+					<button ref={rotateButtonRef} className="popup-button">rotate</button>
+					<button ref={deleteButtonRef} className="popup-button-delete">delete</button>
+				</div>
 
-        {error &&
-          <h1>ERROR</h1>
-        }
+				{error &&
+					<h1>ERROR</h1>
+				}
 
-        {!error &&
-          <div id="mainContent">
-            <Canvas shadows>
-              <Reef coralsData={corals} cursorAvailable={cursorAvail} setCursorAvailable={setCursorAvail} createPopupCallback={createPopup} closePopupCallback={closePopup} deleteCoralCallback={deleteCoral} updateCoralCallback={updateCoral} />
-            </Canvas>
-          </div>
-        }
-      </div >
-    </StrictMode >
-  );
+				{!error &&
+					<div id="mainContent">
+						<Canvas shadows>
+							<Reef coralsData={corals} cursorAvailable={cursorAvail} setCursorAvailable={setCursorAvail} createPopupCallback={createPopup} closePopupCallback={closePopup} deleteCoralCallback={deleteCoral} updateCoralCallback={updateCoral} />
+						</Canvas>
+					</div>
+				}
+			</div >
+		</StrictMode >
+	);
 }
--- client/index.tsx
+++ client/index.tsx
@@ -6,26 +6,29 @@ import './styles.css';
 const root = document.getElementById('root')!;
 const reactRoot = ReactDOM.createRoot(root);
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+function initApp(token: string, id: string) {
+	fetch('http://focusfins.org/api/isAuthenticated', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		}, body: JSON.stringify({ token })
+	}).then(res => {
+		if (res.status === 200) {
+			reactRoot.render(
+				<Dashboard token={token!} id={id!} />
+			);
+		} else {
+			reactRoot.render(
+				<h1 style={{ color: 'white' }}>Not logged in!</h1>
+			);
+		}
+	});
+}
 
-const token = urlParams.get('token');
-const id = urlParams.get('id');
+declare global {
+	interface Window {
+		initApp: (token: string, id: string) => void;
+	}
+};
 
-fetch('./api/isAuthenticated', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  }, body: JSON.stringify({ token })
-}).then(res => {
-  if (res.status === 200) {
-    reactRoot.render(
-      <Dashboard token={token!} id={id!} />
-    );
-  } else {
-    reactRoot.render(
-      <h1 style={{ color: 'white' }}>Not logged in!</h1>
-    );
-  }
-});
-
+window.initApp = initApp;
