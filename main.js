65a27ed296b7d2dfc5fe4845e814d30ac3204cba
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-11-25 00:00:30 -0500

    fixed shit:

--- main.js
+++ main.js
@@ -26,6 +26,12 @@ const app = express();
 app.use(express.json());
 app.use('/public', express.static('./build/public'));
 app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	return next();
+})
+app.use((req, res, next) => {
 	req.secretToken = SECRET_TOKEN;
 	return next();
 });
